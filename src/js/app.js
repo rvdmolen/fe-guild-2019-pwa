48068dd0ce66741962b361425d5c86cb3f629522
or37fw <richard.van.der.molen@ing.com>
2019-01-30 10:58:03 +0100

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,7 @@ const manifest = {
             'sizes': '512x512'
         }
     ],
-    //"start_url": "index.html",
+    //'start_url': '/index.html',
     //"scope": ".",
     'display': 'standalone',
     'orientation': 'portrait-primary',
@@ -48,7 +48,7 @@ const manifest = {
     'dir': 'ltr',
     'lang': 'en-US'
 };
-let deferredPrompt;
+
 
 window.addEventListener('load', () => {
     const base = document.querySelector('base');
@@ -69,6 +69,7 @@ window.addEventListener('load', () => {
     const manifestURL = URL.createObjectURL(blob);
     document.querySelector('#manifestPlaceholder').setAttribute('href', manifestURL);
 
+    // After the existing code
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register(`${baseUrl}sw.js`)
             .then(registration => {
@@ -80,16 +81,5 @@ window.addEventListener('load', () => {
                 console.log('ServiceWorker registration failed: ', err);
             });
     }
-});
 
-window.addEventListener('beforeinstallprompt', event => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    event.preventDefault();
-
-    console.log('beforeinstallprompt fired');
-
-    // Stash the event so it can be triggered later.
-    deferredPrompt = event;
-
-    return false;
 });
