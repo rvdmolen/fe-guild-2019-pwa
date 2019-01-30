dfb13fc1232f5f388ab86612bfe7dfefd595e185
or37fw <richard.van.der.molen@ing.com>
2019-01-30 11:23:41 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+const CACHE_STATIC_NAME = 'static';
+const URLS_TO_PRECACHE = [
+    '/',
+    'index.html',
+    'src/js/app.js',
+    'src/js/feed.js',
+    'src/lib/material.min.js',
+    'src/lib/material.indigo-deep_orange.min.css',
+    'src/css/app.css',
+    'src/css/feed.css',
+    'src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    // 'https://code.getmdl.io/1.3.0/material.indigo-deep_orange.min.css"'
+];
+
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(self.skipWaiting());
@@ -17,4 +33,19 @@ self.addEventListener('fetch', event => {
   }
 
   event.respondWith(fetch(event.request));
+});
+
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+      caches.open(CACHE_STATIC_NAME)
+          .then(cache => {
+              console.log('[Service Worker] Precaching App Shell');
+              cache.addAll(URLS_TO_PRECACHE);
+          })
+          .then(() => {
+              console.log('[ServiceWorker] Skip waiting on install');
+              return self.skipWaiting();
+          })
+  );
 });
